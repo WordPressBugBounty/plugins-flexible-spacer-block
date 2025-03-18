101f42e875c6f15fac6b9c786d170d6007eaf726
Sean Murphy <sean@iamseanmurphy.com>
2025-03-18 02:40:12 +0000

    2.6.0

--- flexible-spacer-block/build/js/index.asset.php
+++ flexible-spacer-block/build/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '7b2c8edf64cf453ec64e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '8b055258147025936fb8');
